d31d8eee8630321275b88e8d4865b872f3b85f24
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-04-21 23:24:46 +0200

    feat: django

--- src/lecture3/lecture3/urls.py
+++ src/lecture3/lecture3/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for leacture3 project.
+"""lecture3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path("hello/", include("hello.urls")),
-    path("newyear/", include("newyear.urls")),
     path("tasks/", include("tasks.urls")),
+    path("newyear/", include("newyear.urls")),
 ]
